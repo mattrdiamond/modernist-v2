66f5b6fe9190c0a898fd385b4995a65529e0ef96
Matt Diamond <mattdiamond80@gmail.com>
2020-02-29 09:48:07 -0600

    Add header component and nav links

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// special syntax in React for importing SVG.
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import "./header.styles.scss";
+
+const Header = () => (
+  <nav className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="/shop">
+        CONTACT
+      </Link>
+    </div>
+  </nav>
+);
+
+export default Header;
