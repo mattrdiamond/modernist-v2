b27603f79f5aef7c7996a61e1ca0f01ffeccbc5b
Matt Diamond <mattdiamond80@gmail.com>
2020-03-18 15:09:13 -0500

    Move shop data to Firebase and add to Redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -17,15 +17,15 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   // 1. Return if user signed out (null)
   if (!userAuth) return;
 
-  // Document reference to user. uid is dynamically generated id that firestore created when user authenticated
+  // Use userAuth obj to query db for documentReference to user. uid is dynamically generated id that firestore created when user authenticated
   const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-  // 2. Query firestore to see if object exists.
+  // 2. Then use documentReference object to get snapShot object, which includes a boolean 'exists' property
   //    Note: .get() returns a snapshot object which represents the data at that location in db,
   //           which we can use to see if userAuth exists
   const snapShot = await userRef.get();
 
-  // 3. If user doesn't exist, create one in that location.
+  // 3. If user doesn't exist, create new user inside userRef (within db).
   //    Note: use document reference object (userRef) to do CRUD operations rather than snapshot
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
@@ -46,6 +46,47 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 };
 
 firebase.initializeApp(config);
+
+// Add collection (shop.collection) and document objects (shop data) to firestore
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  // 1. Create firestore batch object
+  const batch = firestore.batch();
+
+  objectsToAdd.forEach(obj => {
+    // 2. Create a new documentReference object for each category in collection (i.e. hats). Firestore's .doc() method (without params) will generate random IDs which will also be used for url path
+    const newDocRef = collectionRef.doc();
+    // 3. Set the value (title and items) for each docRef object by batching .set() calls together.
+    batch.set(newDocRef, obj);
+  });
+
+  // Execute batch request, which returns a promise. When .commit() succeeds it will resolve a void (null) value
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  // .docs gives us query snapshot, .data() gives us data, only need title and items
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+    // return object from back end that only includes data we need for front end
+    // routeName is a new property who's value will be the same string as title (ie hats). encodeURI - encodes URI by replacing characters that a URL cannot process.
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+  // Convert array of objects to new object. Reduce iterates through each collection in array (ie hats) and adds collection title to new object. It then sets the property value equal to the actual collection object --> hats: {routeName: 'hats', id: 123, etc.}
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
 export const auth = firebase.auth(); // we imported firebase/auth above which gives us access to .auth() method on firebase. Export for anything we need related to authentication
 export const firestore = firebase.firestore();
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,50 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import { connect } from "react-redux";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from "../../firebase/firebase.utils";
+import { updateCollections } from "../../redux/shop/shop.actions";
 
-const ShopPage = ({ match }) => (
-  <div className="shop-page">
-    {/* in this case, match.path will be /shop */}
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+class ShopPage extends React.Component {
+  // snapshot is snapshot representation of collections array from firestore
+  unsubscribeFromSnapshot = null;
 
-export default ShopPage;
+  componentDidMount() {
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection("collections");
+
+    // onSnapshot() subscribes to collectionRef and listens for changes. Will return snapshot representing collections object at the time when this code renders
+    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+      // convert snapshot docs array into new object and only include properties needed for front end
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      // Update reducer with collectionsMap
+      updateCollections(collectionsMap);
+    });
+  }
+
+  render() {
+    const { match } = this.props;
+
+    return (
+      <div className="shop-page">
+        {/* in this case, match.path will be /shop */}
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap =>
+    dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
