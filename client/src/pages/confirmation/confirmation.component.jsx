b6284ffd5ed9b2e35abb732803213892c8bc8a1d
Matt Diamond <mattdiamond80@gmail.com>
2020-09-06 13:23:17 -0500

    Update confirmation component markup and styles

--- client/src/pages/confirmation/confirmation.component.jsx
+++ client/src/pages/confirmation/confirmation.component.jsx
@@ -27,49 +27,121 @@ const Confirmation = (data) => {
             <p>Your payment was processed successfully.</p>
           </div>
           <div className="inner-container">
-            <div className="order-details">
-              <span className="label">Order Number:</span>
-              <span className="details">
-                {Math.round(Math.random() * 10000000000)}
-              </span>
-              <span className="label">Order Date:</span>
-              <span className="details">{orderDate.toLocaleString()}</span>
-              <span className="label">Payment Method:</span>
-              <span className="details">
-                {card.brand} xxxx xxxx xxxx {card.last4}
-              </span>
-              <span className="label">Delivery Details:</span>
-              <span className="details">
-                Standard - Estimated delivery date:{" "}
-                <span>{getDeliveryDate()}</span>
-              </span>
-              <span className="label">Summary:</span>
-              <div className="order-details-inner">
-                <span className="label">Subtotal</span>
-                <span className="details">${cartSubtotal.toFixed(2)}</span>
-                {discount ? (
-                  <>
-                    <span className="label promo">Promo</span>
-                    <span className="details promo">
-                      -${discount.toFixed(2)}
-                    </span>
-                  </>
-                ) : null}
-                <span className="label hr-above">Sales Tax</span>
-                <span className="details hr-above">${tax.toFixed(2)}</span>
-                <span className="label">Shipping</span>
-                <span className="details">${shipping.toFixed(2)}</span>
-                <span className="label hr-above">Total</span>
-                <span className="details hr-above">
-                  ${(amount / 100).toFixed(2)}
+            <ul className="order-details">
+              <li className="order-detail">
+                <span className="label">Order Number:</span>
+                <span className="details">
+                  {Math.round(Math.random() * 10000000000)}
                 </span>
-              </div>
-            </div>
+              </li>
+              <li className="order-detail">
+                <span className="label">Order Date:</span>
+                <span className="details">{orderDate.toLocaleString()}</span>
+              </li>
+              <li className="order-detail">
+                <span className="label">Payment Method:</span>
+                <span className="details">
+                  {card.brand} xxxx xxxx xxxx {card.last4}
+                </span>
+              </li>
+              <li className="order-detail">
+                <span className="label">Delivery Details:</span>
+                <span className="details">
+                  Standard - Estimated delivery date:{" "}
+                  <span>{getDeliveryDate()}</span>
+                </span>
+              </li>
+              <li className="order-detail">
+                <span className="label">Summary:</span>
+                <ul className="order-details-inner">
+                  <li className="subtotal">
+                    <span className="label">Subtotal</span>
+                    <span className="details">${cartSubtotal.toFixed(2)}</span>
+                  </li>
+                  {discount ? (
+                    <li className="promo">
+                      <span className="label">Promo</span>
+                      <span className="details">-${discount.toFixed(2)}</span>
+                    </li>
+                  ) : null}
+                  <li className="tax">
+                    <span className="label">Sales Tax</span>
+                    <span className="details">${tax.toFixed(2)}</span>
+                  </li>
+                  <li className="shipping">
+                    <span className="label">Shipping</span>
+                    <span className="details">${shipping.toFixed(2)}</span>
+                  </li>
+                  <li className="total">
+                    <span className="label">Total</span>
+                    <span className="details">
+                      ${(amount / 100).toFixed(2)}
+                    </span>
+                  </li>
+                </ul>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
     </div>
   );
+  // return (
+  //   <div className="confirmation-page">
+  //     <div className="page-width">
+  //       <div className="inner-window">
+  //         <div className="inner-container">
+  //           <h2 className="title">Thanks for shopping with us!</h2>
+  //           <p>Your payment was processed successfully.</p>
+  //         </div>
+  //         <div className="inner-container">
+  //           <div className="order-details">
+  //             <span className="label">Order Number:</span>
+  //             <span className="details">
+  //               {Math.round(Math.random() * 10000000000)}
+  //             </span>
+  //             <span className="label">Order Date:</span>
+  //             <span className="details">{orderDate.toLocaleString()}</span>
+  //             <span className="label">Payment Method:</span>
+  //             <span className="details">
+  //               {card.brand} xxxx xxxx xxxx {card.last4}
+  //             </span>
+  //             <span className="label">Delivery Details:</span>
+  //             <span className="details">
+  //               Standard - Estimated delivery date:{" "}
+  //               <span>{getDeliveryDate()}</span>
+  //             </span>
+  //             <span className="label">Summary:</span>
+  //             <ul className="order-details-inner">
+  //               <li className="subtotal">
+  //                 <span className="label">Subtotal</span>
+  //                 <span className="details">${cartSubtotal.toFixed(2)}</span>
+  //               </li>
+  //               {discount ? (
+  //                 <li className="promo">
+  //                   <span className="label">Promo</span>
+  //                   <span className="details">-${discount.toFixed(2)}</span>
+  //                 </li>
+  //               ) : null}
+  //               <li className="tax">
+  //                 <span className="label">Sales Tax</span>
+  //                 <span className="details">${tax.toFixed(2)}</span>
+  //               </li>
+  //               <li className="shipping">
+  //                 <span className="label">Shipping</span>
+  //                 <span className="details">${shipping.toFixed(2)}</span>
+  //               </li>
+  //               <li className="total">
+  //                 <span className="label">Total</span>
+  //                 <span className="details">${(amount / 100).toFixed(2)}</span>
+  //               </li>
+  //             </ul>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default withRouter(Confirmation);
