8248dc18a363394558adffa9a38bb7e586ed9c61
mattrdiamond <mattdiamond80@gmail.com>
2023-12-07 22:48:55 -0600

    update directory images

--- client/src/components/menu-item/menu-item.component.jsx
+++ client/src/components/menu-item/menu-item.component.jsx
@@ -1,28 +1,27 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import ResponsiveImage from "../responsive-image/responsive-image.component";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({
-  title,
-  linkUrl,
-  history,
-  match,
-  images: { large, small, mobile2x, mobile1x },
-}) => (
-  <div
-    className='menu-item'
-    onClick={() => history.push(`${match.url}${linkUrl}`)}
-  >
-    <picture>
-      <source media='(min-width: 701px)' srcSet={`${large} 2x, ${small} 1x`} />
-      <source srcSet={`${mobile2x} 2x, ${mobile1x} 1x`} />
-      <img className='background-image' alt={title} src={small} />
-    </picture>
-    <div className='content'>
-      <h2 className='title'>{title}</h2>
-      <span className='shop-link'>Shop Now</span>
+const MenuItem = ({ title, linkUrl, history, match, images }) => {
+  return (
+    <div
+      className='menu-item'
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <ResponsiveImage
+        picture={{
+          sources: images,
+          alt: title,
+          styles: "background-image",
+        }}
+      />
+      <div className='content'>
+        <h2 className='title'>{title}</h2>
+        <span className='shop-link'>Shop Now</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default withRouter(MenuItem);
