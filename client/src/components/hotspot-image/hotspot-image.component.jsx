baa8883676ea1eb2c202ad1944e2d1fa291787ba
mattrdiamond <mattdiamond80@gmail.com>
2024-02-23 15:58:09 -0600

    remove unused vars

--- client/src/components/hotspot-image/hotspot-image.component.jsx
+++ client/src/components/hotspot-image/hotspot-image.component.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import useIntersectionObserver from "../../hooks/use-intersection-observer";
 import { useWindowSize } from "../../contexts/WindowSizeContext";
